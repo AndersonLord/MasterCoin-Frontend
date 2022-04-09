6da9327a48f5997c275887ef83fd2922b6a83fc5
Anderson Ferreira <andersonolivera786@gmail.com>
2022-04-09 15:51:31 -0300

    Teste Build

--- next.config.js
+++ next.config.js
@@ -1,45 +1,4 @@
-const withSass = require("@zeit/next-sass");
-const withLess = require("@zeit/next-less");
-const withCSS = require("@zeit/next-css");
-
-const isProd = process.env.NODE_ENV === "production";
-
-// fix: prevents error when .less files are required by node
-if (typeof require !== "undefined") {
-  require.extensions[".less"] = (file) => {};
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
 }
-
-module.exports = withCSS({
-  target: 'serverless',
-  exportPathMap: () => ({
-    "/": {
-      page: "/",
-    },
-  }),
-  assetPrefix: isProd ? "" : "",
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-  },
-  webpack: (config, {
-    dev
-  }) => {
-    // Perform customizations to webpack config
-    config.module.rules = config.module.rules.map((rule) => {
-      if (rule.loader === "babel-loader") {
-        rule.options.cacheDirectory = false;
-      }
-      return rule;
-    });
-
-    return config;
-  },
-  ...withLess(
-    withSass({
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-      },
-    })
-  ),
-});
